8ecb52d6095e4a88b5f04e58697052e90599ad06
Yahton <>
2024-03-03 16:32:39 +0300

    big update

--- my_commands.py
+++ my_commands.py
@@ -4,43 +4,30 @@ from discord import app_commands
 from gigachat import GigaChat
 
 import config
+import json
+import os
 
 
-class MyCog(commands.Cog):
+class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(description="Задать вопрос Gigachat")
     @app_commands.describe(request="Ваш запрос тут")
     async def ai(self, interaction: discord.Interaction, request: str) -> None:
-        await interaction.response.send_message("Загрузка...")
-        with GigaChat(
-            credentials=config.GIGACHAT_TOKEN, verify_ssl_certs=False
-        ) as giga:
-            response = giga.chat(request)
-            await interaction.edit_original_response(
-                content=response.choices[0].message.content
-            )
-
-    @commands.Cog.listener()
-    async def on_voice_state_update(self, member, before, after):
-        if before and before.channel:
-            if (
-                not before.channel.members
-                and before.channel.category.id == config.TEMPORARY_CHANNELS_CATEGORY_ID
-            ):
-                await before.channel.delete()
-
-        if after:
-            if after.channel.id == config.CREATE_TEMPORARY_CHANNEL_ID:
-                guild = after.channel.guild
-                category = discord.utils.get(
-                    guild.categories, id=config.TEMPORARY_CHANNELS_CATEGORY_ID
+        if config.GIGACHAT_TOKEN:
+            await interaction.response.send_message("Загрузка...")
+            with GigaChat(
+                credentials=config.GIGACHAT_TOKEN, verify_ssl_certs=False
+            ) as giga:
+                response = giga.chat(request)
+                await interaction.edit_original_response(
+                    content=response.choices[0].message.content
                 )
-                channel = await guild.create_voice_channel(
-                    f"канал {member.display_name}", category=category
-                )
-                await member.move_to(channel)
+        else:
+            await interaction.response.send_message("Создатель бота жмот, который не удосужился дать мне API ключ :/")
+    
+
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -48,5 +35,61 @@ class MyCog(commands.Cog):
             return
 
 
+class VoiceMaster(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.channel_creators = {}
+        self.temporary_channels = []
+        self.data_load()
+        print(self.channel_creators)
+    
+    def data_load(self):
+        if os.path.exists('data/channels_creators.json'):
+            with open("data/channels_creators.json", 'r') as data:
+                self.channel_creators = json.load(data)
+        if os.path.exists('data/channels.json'):
+            with open("data/channels.json", 'r') as data:
+                self.temporary_channels = json.load(data)
+    
+    def data_save(self):
+        with open("data/channels_creators.json", "w") as outfile:
+            json.dump(self.channel_creators, outfile)
+        with open("data/channels.json", "w") as outfile:
+            json.dump(self.temporary_channels, outfile)
+
+    @app_commands.command(description="Создать канал-творец")
+    @commands.has_permissions(administrator = True)
+    async def setup(self, interaction: discord.Interaction):
+        self.channel_creators.update({interaction.guild.id: (await interaction.guild.create_voice_channel("Создать приватку SB")).id})
+        await interaction.response.send_message("Канал-творец создан!")
+        self.data_save()
+        
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+        if before.channel:
+            if (
+                not before.channel.members
+                and before.channel.id in self.temporary_channels
+            ):
+                self.temporary_channels.remove(before.channel.id)
+                await before.channel.delete()
+                self.data_save()
+
+        if after.channel:
+            if after.channel.id == self.channel_creators.get(str(after.channel.guild.id), -1):
+                guild: discord.Guild = after.channel.guild
+                category = discord.utils.get(after.channel.guild.channels, id=self.channel_creators.get(str(after.channel.guild.id), 0))
+                channel = await guild.create_voice_channel(
+                    f"канал {member.display_name}", category=category.category if category else None
+                )
+                await member.move_to(channel)
+                self.temporary_channels.append(channel.id)
+                self.data_save()
+
+                await channel.set_permissions(member, manage_channels=True, mute_members=True, move_members=True, manage_roles=True)
+
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(MyCog(bot))
+    await bot.add_cog(General(bot))
+    await bot.add_cog(VoiceMaster(bot))
+
