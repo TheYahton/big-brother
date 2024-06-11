b73dd47713d80230d764a7ba55f33e8b31b757ac
TheYahton <>
2024-06-11 20:48:29 +0000

    fix: attachments logging

--- my_commands.py
+++ my_commands.py
@@ -15,6 +15,16 @@ def should_logged(message: discord.Message):
         return True
 
 
+async def copy_attachments(message: discord.Message) -> list[discord.Attachment] | None:
+    if achts := message.attachments:
+        files = []
+        for file in achts:
+            files.append(await file.to_file(use_cached=True))
+        return files
+
+    return None
+
+
 class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -25,22 +35,24 @@ class General(commands.Cog):
         if not should_logged(message_after):
             return
 
-        description = f"{message_before.content}{''.join([attachment.url for attachment in message_before.attachments])}\
-            \nTo:\n{message_after.content}{''.join([attachment.url for attachment in message_after.attachments])}"
+        description = f"{message_before.content}\
+            \nTo:\n{message_after.content}"
         embed = discord.Embed(description=description, color=discord.Color.yellow(), url=message_after.jump_url, title=message_after.channel.name)
         embed.set_author(name=message_after.author.name, icon_url=message_after.author.avatar)
         embed.set_footer(text="#edited")
-        await self.logs_channel.send(embed=embed)
+        files = await copy_attachments(message_before)
+        await self.logs_channel.send(embed=embed, files=files)
 
     @commands.Cog.listener()
     async def on_message_delete(self, message: discord.Message) -> None:
         if not should_logged(message):
             return
 
-        embed = discord.Embed(description=f"{message.content}{''.join([attachment.url for attachment in message.attachments])}", color=discord.Color.red(), url=message.jump_url, title=message.channel.name)
+        embed = discord.Embed(description=message.content, color=discord.Color.red(), url=message.jump_url, title=message.channel.name)
         embed.set_author(name=message.author.name, icon_url=message.author.avatar)
         embed.set_footer(text="#deleted")
-        await self.logs_channel.send(embed=embed)
+        files = await copy_attachments(message)
+        await self.logs_channel.send(embed=embed, files=files)
 
 
 class VoiceMaster(commands.Cog):
