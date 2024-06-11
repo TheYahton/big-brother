259ea5b9b0a1d15eb8e137147a479cde6fee7a8e
TheYahton <>
2024-06-11 21:02:51 +0000

    очень устал

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 
 TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_CREATOR_ID = os.getenv("CREATOR_ID")
-CHANNEL_LOGS_ID = os.getenv("LOGS_ID")
+CHANNEL_CREATOR_ID = int(os.getenv("CREATOR_ID"))
+CHANNEL_LOGS_ID = int(os.getenv("LOGS_ID"))
 LOG_DEBUG = os.getenv("BOT_DEBUG")
